28a3db58fcbce1b60e2f7c23ac0ed84e728a051b
Kamil Enl <enlightenmentsoftware@gmail.com>
2024-07-17 21:05:47 +0200

    Pseudo code Add to test compatybility libs - I

--- testConn/session.php
+++ testConn/session.php
@@ -1,2 +1,10 @@
 <?php
+
 //php encrypt key by jsencrypt key and send AES key
+
+require_once( __DIR__ . "/server/class/EnlAes.php");
+require_once( __DIR__ . "/server/class/EnlRsa.php");
+
+if( isset($_GET['pub']) ){
+    $pub = $_GET['pub'];
+}
